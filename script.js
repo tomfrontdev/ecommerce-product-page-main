a316d33c2f4a73b490814a5273b82ca818b050dd
Tomasz Cividino <tomaszcividino@gmail.com>
2022-06-09 19:36:17 +0200

    Refactor script

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ const discountPrice = document.querySelector(".price__amount--discountedprice");
 const originalPrice = document.querySelector(".price__amount--originalprice");
 const amount = document.querySelector(".amount__number");
 const addtoCart = document.querySelector(".quantity__div--addtoCart");
-const navMenu = document.querySelector(".navmobile__hamburger");
-const navMenuBar = document.querySelector(".navmobile__dropdownnav");
-const navcartIcon = document.querySelector(".navmobile__cart");
+const navMenu = document.querySelector(".nav__hamburger");
+const navMenuBar = document.querySelector(".nav__dropdownnav");
+const navcartIcon = document.querySelector(".nav__cart");
 const cartPopup = document.querySelector(".cart__popup--orange");
 const cartPopupamount = document.querySelector(".cart__popup--amount");
 const sliderCart = document.querySelector(".slider__cart");
@@ -27,7 +27,7 @@ const sliderCartorderprice = document.querySelector(".cartproduct__orderprice");
 const sliderCartorderspan = document.querySelector(
 	".cartproduct__orderprice--bold"
 );
-const translucentBackground = document.querySelector(".navmobile__background");
+const translucentBackground = document.querySelector(".nav__background");
 const cartOrderdelete = document.querySelector(".cartproduct__deleteicon");
 const navmenuClose = document.querySelector(".dropdownnav__closeicon");
 
@@ -36,92 +36,86 @@ let priceIndex = 1;
 let flag = true;
 let imgId = 1;
 
-navMenu.addEventListener("click", () => {
-	hideCart();
-	showAnimation();
-	translucentBackground.classList.remove("hidden");
-});
-
-if (window.outerWidth < 500) {
-	activeSlide.forEach((s, i) => {
-		s.style.transform = `translateX(${100 * i}%)`;
-	});
-}
-
-if (window.outerWidth > 500) {
-	activeSlide.forEach((slide) => {
-		slide.addEventListener("click", (event) => {
-			imgId = event.target.dataset.info;
-			if (flag) {
-				desktopImg.src = `images/image-product-${imgId}.jpg`;
+showMobileCart = () => {
+	navcartIcon.addEventListener("click", (e) => {
+		if (!navMenuBar.classList.contains("showanimation")) {
+			toggleCart();
+			showOrder();
+		}
+		if (navMenuBar.classList.contains("showanimation")) {
+			hideAnimation();
+			if (sliderCart.classList.contains("hidden")) {
+				showOrder();
+				sliderCart.classList.remove("hidden");
 			}
-			if (!flag) {
-				lightboxImg.src = `images/image-product-${imgId}.jpg`;
-			}
-			event.target.classList.add("clicked");
-			thumbnailImg.forEach((img) => {
-				if (img.dataset.info !== imgId) {
-					img.classList.remove("clicked");
-				}
-			});
-		});
+		}
 	});
-}
+};
 
-const moveSlider = () => {
+switchImg = () => {
+	document.querySelector(`[data-info="${imgId}"]`).classList.add("clicked");
+	thumbnailImg.forEach((img) => {
+		if (img.dataset.info != imgId) {
+			img.classList.remove("clicked");
+		}
+	});
+	lightboxImg.src = `images/image-product-${imgId}.jpg`;
+};
+
+moveSlider = () => {
 	activeSlide.forEach(
 		(s, i) =>
 			(s.style.transform = `translateX(${(i - currentSlide) * 100}%)`)
 	);
 };
 
-const nextSlider = () => {
+nextSlider = () => {
 	moveSlider();
 	currentSlide++;
 	if (currentSlide >= 4) currentSlide = 0;
 };
 
-const prevSlider = () => {
+prevSlider = () => {
 	moveSlider();
 	currentSlide--;
 	if (currentSlide == -1) currentSlide = 3;
 };
 
-const setPrices = () => {
+setPrices = () => {
 	discountPrice.textContent = `${125 * amount.textContent}.00$`;
 	originalPrice.textContent = `${250 * amount.textContent}.00$`;
 };
 
-const emptyCart = () => {
+emptyCart = () => {
 	sliderCarttext.classList.remove("hidden");
 	sliderCartorder.classList.add("hidden");
 };
 
-const filledCart = () => {
+filledCart = () => {
 	sliderCarttext.classList.add("hidden");
 	sliderCartorder.classList.remove("hidden");
 };
 
-const showAnimation = () => {
+showAnimation = () => {
 	navMenuBar.classList.add("showanimation");
 	navMenuBar.classList.remove("hideanimation");
 };
 
-const hideAnimation = () => {
+hideAnimation = () => {
 	navMenuBar.classList.remove("showanimation");
 	navMenuBar.classList.add("hideanimation");
 };
 
-const showOrder = () => {
+showOrder = () => {
 	if (cartPopup.classList.contains("hidden")) emptyCart();
 	if (!cartPopup.classList.contains("hidden")) filledCart();
 };
 
-const hideCart = () => {
+hideCart = () => {
 	sliderCart.classList.add("hidden");
 };
 
-const toggleCart = () => {
+toggleCart = () => {
 	sliderCart.classList.toggle("hidden");
 };
 
@@ -135,38 +129,6 @@ addtoCart.addEventListener("click", () => {
 		125 * cartPopupamount.textContent
 	}</span>`;
 });
-
-if (window.outerWidth < 500) {
-	navcartIcon.addEventListener("click", (e) => {
-		if (!navMenuBar.classList.contains("showanimation")) {
-			toggleCart();
-			showOrder();
-		}
-		if (navMenuBar.classList.contains("showanimation")) {
-			hideAnimation();
-			if (sliderCart.classList.contains("hidden")) {
-				showOrder();
-				sliderCart.classList.remove("hidden");
-			}
-		}
-	});
-}
-
-if (window.outerWidth > 500) {
-	navcartIcon.addEventListener("click", (e) => {
-		if (!navMenuBar.classList.contains("showanimation")) {
-			toggleCart();
-			showOrder();
-		}
-		if (navMenuBar.classList.contains("showanimation")) {
-			hideAnimation();
-			if (sliderCart.classList.contains("hidden")) {
-				showOrder();
-				sliderCart.classList.remove("hidden");
-			}
-		}
-	});
-}
 
 cartOrderdelete.addEventListener("click", () => {
 	cartPopupamount.textContent = 0;
@@ -197,24 +159,10 @@ navmenuClose.addEventListener("click", () => {
 	translucentBackground.classList.add("hidden");
 });
 
-document.body.addEventListener("click", (e) => {
-	if (!sliderCart.classList.contains("hidden")) {
-		if (
-			!e.target.closest(".slider__cart") &&
-			!e.target.closest(".navmobile__cart") &&
-			!e.target.closest(".content__amountbtn")
-		)
-			hideCart();
-	}
-	if (navMenuBar.classList.contains("showanimation")) {
-		if (
-			!e.target.closest(".navmobile__dropdownnav") &&
-			!e.target.closest(".navmobile__hamburger")
-		) {
-			hideAnimation();
-			translucentBackground.classList.add("hidden");
-		}
-	}
+navMenu.addEventListener("click", () => {
+	hideCart();
+	showAnimation();
+	translucentBackground.classList.remove("hidden");
 });
 
 desktopImg.addEventListener("click", () => {
@@ -236,23 +184,58 @@ lightboxClose.addEventListener("click", () => {
 prevBtnLightbox.addEventListener("click", () => {
 	imgId--;
 	if (imgId == 0) imgId = 4;
-	document.querySelector(`[data-info="${imgId}"]`).classList.add("clicked");
-	thumbnailImg.forEach((img) => {
-		if (img.dataset.info != imgId) {
-			img.classList.remove("clicked");
-		}
-	});
-	lightboxImg.src = `images/image-product-${imgId}.jpg`;
+	switchImg();
 });
 
 nextBtnLightbox.addEventListener("click", () => {
 	imgId++;
 	if (imgId > 4) imgId = 1;
-	document.querySelector(`[data-info="${imgId}"]`).classList.add("clicked");
-	thumbnailImg.forEach((img) => {
-		if (img.dataset.info != imgId) {
-			img.classList.remove("clicked");
-		}
+	switchImg();
+});
+
+if (window.outerWidth < 500) {
+	activeSlide.forEach((s, i) => {
+		s.style.transform = `translateX(${100 * i}%)`;
 	});
-	lightboxImg.src = `images/image-product-${imgId}.jpg`;
+	showMobileCart();
+}
+
+if (window.outerWidth > 500) {
+	activeSlide.forEach((slide) => {
+		slide.addEventListener("click", (event) => {
+			imgId = event.target.dataset.info;
+			if (flag) {
+				desktopImg.src = `images/image-product-${imgId}.jpg`;
+			}
+			if (!flag) {
+				lightboxImg.src = `images/image-product-${imgId}.jpg`;
+			}
+			event.target.classList.add("clicked");
+			thumbnailImg.forEach((img) => {
+				if (img.dataset.info !== imgId) {
+					img.classList.remove("clicked");
+				}
+			});
+		});
+	});
+}
+
+document.body.addEventListener("click", (e) => {
+	if (!sliderCart.classList.contains("hidden")) {
+		if (
+			!e.target.closest(".slider__cart") &&
+			!e.target.closest(".nav__cart") &&
+			!e.target.closest(".content__amountbtn")
+		)
+			hideCart();
+	}
+	if (navMenuBar.classList.contains("showanimation")) {
+		if (
+			!e.target.closest(".nav__dropdownnav") &&
+			!e.target.closest(".nav__hamburger")
+		) {
+			hideAnimation();
+			translucentBackground.classList.add("hidden");
+		}
+	}
 });
